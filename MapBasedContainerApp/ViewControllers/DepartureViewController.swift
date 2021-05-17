72066ee2d050afaf1e5fe7d5a9b70657dcb8aac5
Jisu Bae <jb0819@gmail.com>
2021-05-17 22:53:41 +0900

    Add name label and floating panel layout

--- MapBasedContainerApp/ViewControllers/DepartureViewController.swift
+++ MapBasedContainerApp/ViewControllers/DepartureViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 import FloatingPanel
 
 class DepartureViewController: UIViewController {
+    private lazy var viewNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 15)
+        label.text = "\(DepartureViewController.self)"
+        label.backgroundColor = .white
+        
+        return label
+    }()
+    
     private lazy var fpControl = FloatingPanelController()
     
     override func loadView() {
@@ -27,13 +36,37 @@ class DepartureViewController: UIViewController {
     private func configureViews() {
         view.backgroundColor = .clear
         
-        fpControl.delegate = self
-        fpControl.set(contentViewController: VirtualStopPickerViewController())
+        let pickerViewController = VirtualStopPickerViewController()
+        pickerViewController.listener = self.parent
+        fpControl.set(contentViewController: pickerViewController)
+        
+        fpControl.layout = CustomFloatingPanelLayout()
         fpControl.addPanel(toParent: self, animated: false)
         fpControl.surfaceView.backgroundColor = .white
-    }
+        
+        view.addSubview(viewNameLabel)
+        viewNameLabel.snp.makeConstraints { maker in
+            maker.leading.equalToSuperview().inset(16)
+            maker.bottom.equalTo(fpControl.surfaceView.snp.top).offset(-10)
+        }
+    }    
 }
 
-extension DepartureViewController: FloatingPanelControllerDelegate {
-    
+extension DepartureViewController {
+    class CustomFloatingPanelLayout: FloatingPanelLayout {
+        var position: FloatingPanelPosition = .bottom
+        var initialState: FloatingPanelState = .half
+
+        var anchors: [FloatingPanelState: FloatingPanelLayoutAnchoring] {
+            return [
+                .full: FloatingPanelLayoutAnchor(absoluteInset: 350.0, edge: .bottom, referenceGuide: .safeArea),
+                .half: FloatingPanelLayoutAnchor(absoluteInset: 150.0, edge: .bottom, referenceGuide: .safeArea),
+            ]
+        }
+
+        func backdropAlpha(for state: FloatingPanelState) -> CGFloat {
+            return 0.0
+        }
+    }
+
 }
--- MapBasedContainerApp/ViewControllers/DestinationViewController.swift
+++ MapBasedContainerApp/ViewControllers/DestinationViewController.swift
@@ -6,8 +6,67 @@
 //
 
 import UIKit
+import FloatingPanel
 
 class DestinationViewController: UIViewController {
+    private lazy var viewNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 15)
+        label.text = "\(DestinationViewController.self)"
+        label.backgroundColor = .systemGray5
+        
+        return label
+    }()
     
+    private lazy var fpControl = FloatingPanelController()
+    
+    override func loadView() {
+        super.loadView()
+        
+        let mainFrame = UIScreen.main.bounds
+        view = PassThroughView(frame: mainFrame)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureViews()
+    }
+    
+    private func configureViews() {
+        view.backgroundColor = .clear
+        
+        let pickerViewController = VirtualStopPickerViewController()
+        pickerViewController.listener = self.parent
+        fpControl.set(contentViewController: pickerViewController)
+        
+        fpControl.layout = CustomFloatingPanelLayout()
+        fpControl.addPanel(toParent: self, animated: false)
+        fpControl.surfaceView.backgroundColor = .systemGray5
+        
+        view.addSubview(viewNameLabel)
+        viewNameLabel.snp.makeConstraints { maker in
+            maker.leading.equalToSuperview().inset(16)
+            maker.bottom.equalTo(fpControl.surfaceView.snp.top).offset(-10)
+        }
+    }
 }
 
+extension DestinationViewController {
+    class CustomFloatingPanelLayout: FloatingPanelLayout {
+        var position: FloatingPanelPosition = .bottom
+        var initialState: FloatingPanelState = .half
+
+        var anchors: [FloatingPanelState: FloatingPanelLayoutAnchoring] {
+            return [
+                .full: FloatingPanelLayoutAnchor(absoluteInset: 500.0, edge: .bottom, referenceGuide: .safeArea),
+                .half: FloatingPanelLayoutAnchor(absoluteInset: 250.0, edge: .bottom, referenceGuide: .safeArea),
+            ]
+        }
+
+        func backdropAlpha(for state: FloatingPanelState) -> CGFloat {
+            return 0.0
+        }
+    }
+
+}
