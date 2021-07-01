859c02d306bae994c3308cddd249f5c52463364e
Jisu Bae <jb0819@gmail.com>
2021-07-01 23:50:06 +0900

    Add comment

--- MapBasedContainerApp/Views/PassThroughView.swift
+++ MapBasedContainerApp/Views/PassThroughView.swift
@@ -10,7 +10,8 @@ import UIKit
 class PassThroughView: UIView {
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         let hitView = super.hitTest(point, with: event)
-        
+
+        // return nil
         if hitView == self {
             return nil
         } else {
